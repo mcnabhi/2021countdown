554cb27d759ff651085f874cac7849341c4a24eb
mcnabhi <74385984+mcnabhi@users.noreply.github.com>
2020-12-21 13:33:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,30 +4,20 @@ var menu = document.getElementById('freeze');
 var nav = document.getElementById('nav');
 var resume = document.getElementById('resume');
 
-            freeze.addEventListener('click', function(e) {
-                nav.classList.toggle('hide-mobile');
-                e.preventDefault();
-                clearInterval(x)
-            });
+resume.addEventListener('click', function(e) {
+    nav.classList.add('hide-mobile');
+    e.preventDefault();
+    location.reload()
 
-            resume.addEventListener('click', function(e) {
-                nav.classList.add('hide-mobile');
-                e.preventDefault();
-                var deadline = new Date("dec 31, 2020 23:59:59").getTime(); 
-  
-                    var now = new Date().getTime(); 
-                    var t = deadline - now; 
-                    var days = Math.floor(t / (1000 * 60 * 60 * 24)); 
-                    var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60)); 
-                    var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60)); 
-                    var seconds = Math.floor((t % (1000 * 60)) / 1000); 
-                    document.getElementById("day").innerHTML =days ; 
-                    document.getElementById("hour").innerHTML =hours; 
-                    document.getElementById("minute").innerHTML = minutes;  
-                    document.getElementById("second").innerHTML =seconds;  
-            });
+});
 
-  
+freeze.addEventListener('click', function(e){
+               
+    nav.classList.toggle('hide-mobile');
+    e.preventDefault();
+    clearInterval(x) 
+});
+
 var x = setInterval(function() { 
 
   
@@ -41,8 +31,6 @@ document.getElementById("day").innerHTML =days ;
 document.getElementById("hour").innerHTML =hours; 
 document.getElementById("minute").innerHTML = minutes;  
 document.getElementById("second").innerHTML =seconds;  
-
-
 
 if (t < 0) { 
 
@@ -58,5 +46,3 @@ if (t < 0) {
 
         document.getElementById("second").innerHTML = '0'; } 
 }, 1000); 
-
-
